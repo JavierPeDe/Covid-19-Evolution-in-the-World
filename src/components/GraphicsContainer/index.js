a886d229625f3aab5938920f4710d948e5288d9e
javierPerez <javip107@gmail.com>
2021-07-09 10:27:13 +0200

    feat: Responsive graphics & loading gif

--- src/components/GraphicsContainer/index.js
+++ src/components/GraphicsContainer/index.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import { LineChart } from './LineChart';
 import { useDataByCountry, useDataByDateRangeByCountry } from '../../hooks';
+import imgLoading from '../../assets/img/careers-loading.gif';
+import style from './index.module.css';
 export const GraphicsContainer = ({ country }) => {
-  const { responseRange, error, loading } = useDataByCountry('ES');
+  const { responseRange, error, loading } = useDataByCountry('DE');
 
   return (
-    <div>
-      <div>
-        <LineChart
-          infected={loading ? [] : responseRange.infected}
-          deaths={loading ? [] : responseRange.deaths}
-          recovered={loading ? [] : responseRange.recovery}
-          dates={loading ? [] : responseRange.dates}
-        />
+    <div className={style.graphicContainer}>
+      <div className={style.graphic}>
+        <h1> Graphic asdlfkhnsaldkfhj</h1>
+        {loading ? (
+          <img src={imgLoading} />
+        ) : (
+          <LineChart
+            className=""
+            infected={responseRange.infected}
+            deaths={responseRange.deaths}
+            recovered={responseRange.recovery}
+            dates={responseRange.dates}
+          />
+        )}
       </div>
-      <div>
-        <LineChart
-          infected={loading ? [] : responseRange.newInfected}
-          deaths={loading ? [] : responseRange.newDeaths}
-          recovered={loading ? [] : responseRange.newRecovered}
-          dates={loading ? [] : responseRange.dates}
-        />
+      <div className={style.graphic}>
+        <h1>Graphic oasdhfopajshdgplo</h1>
+        {loading ? (
+          <img src={imgLoading} />
+        ) : (
+          <LineChart
+            infected={responseRange.newInfected}
+            deaths={responseRange.newDeaths}
+            recovered={responseRange.newRecovered}
+            dates={responseRange.dates}
+          />
+        )}
       </div>
     </div>
   );
