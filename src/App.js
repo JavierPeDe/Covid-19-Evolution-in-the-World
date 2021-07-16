e96e8d150121c24ca15531bb623b68b7d143eb44
javierPerez <javip107@gmail.com>
2021-07-16 10:23:06 +0200

    feat: navBar & react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
 import { Home } from './components/Home';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { Paper, Switch, Typography } from "@material-ui/core";
+import { Info } from './components/Info';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import {
+  Paper,
+  Switch,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch as SwitchRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
+import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import styles from './App.module.css';
+import { classes } from 'istanbul-lib-coverage';
 
 export const App = () => {
   const [dark, setDark] = useState(false);
@@ -11,17 +31,82 @@ export const App = () => {
     palette: {
       type: dark ? 'dark' : 'light',
     },
-  })
+  });
 
   return (
-    <div className={styles.main}>
-      <ThemeProvider theme={theme}>
-        <Paper>
-          <Switch checked={dark} className={styles.darkMode} onChange={() => setDark(!dark)} />
-          <Typography className={styles.mainTitle} variant="h1">Covid 19</Typography>
-          <Home />
-        </Paper>
-      </ThemeProvider>
-    </div>
+    <Router>
+      <div className={styles.main}>
+        <ThemeProvider theme={theme}>
+          <Paper>
+            <Drawer
+              style={{ width: ' 70px' }}
+              variant="persistent"
+              anchor="top"
+              open={true}
+              classes={{ paper: theme.MuiDrawer }}
+            >
+              <List
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-around',
+                  alignItems: 'center',
+                  textDecoration: 'none',
+                }}
+              >
+                <Link to="/" className={classes.link}>
+                  <ListItem button>
+                    <ListItemIcon>
+                      <AssessmentOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'Home'} />
+                  </ListItem>
+                </Link>
+                <Link to="/about" className={classes.link}>
+                  <ListItem button>
+                    <ListItemIcon>
+                      <InfoOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'About'} />
+                  </ListItem>
+                </Link>
+                <ListItem
+                  button
+                  component="a"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/JavierPeDe/Covid-19-Evolution-in-the-World"
+                >
+                  <ListItemIcon>
+                    <GitHubIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={'Github'} />
+                </ListItem>
+                <ListItem>
+                  <h3 style={{ margin: '0' }}>Evolution of Covid-19</h3>
+                </ListItem>
+                <ListItem>
+                  <Switch
+                    checked={dark}
+                    className={styles.darkMode}
+                    onChange={() => setDark(!dark)}
+                  />
+                </ListItem>
+              </List>
+            </Drawer>
+
+            {/* <Typography className={styles.mainTitle} variant="h1">
+              Covid 19
+            </Typography> */}
+            <Route exact path="/">
+              <div className={styles.homeContaine}> </div>
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <Info />
+            </Route>
+          </Paper>
+        </ThemeProvider>
+      </div>
+    </Router>
   );
 };
